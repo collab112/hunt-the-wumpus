6950862505f5521c55fc69abaaddc9fc6f03004a
ardnMCER <53517638+collab112@users.noreply.github.com>
2022-07-25 03:03:33 +1000

    Smallchange-1

--- source-code/Wumpus/WumpusStates.cs
+++ source-code/Wumpus/WumpusStates.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+using UnityEngine.AI; // Needed for working with AI
 
 public class WumpusStates : MonoBehaviour
 {
